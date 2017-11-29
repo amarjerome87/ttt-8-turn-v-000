691339209a3ba745f27a5034759b749e843bb8e7
Amar Chatman <amarjerome@gmail.com>
2017-11-29 23:46:29 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,7 +25,7 @@ def turn(board)
   if valid_move?(board,index)
     move(board, index, current_player = "X")
   else
-    puts "Please enter 1-9:"
+    turn(board) 
   end
   display_board(board)
 end
